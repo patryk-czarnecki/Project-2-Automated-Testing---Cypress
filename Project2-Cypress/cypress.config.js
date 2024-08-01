2e7fb0df738aaec72399164967499820a9f1216e
Patryk Czarnecki <pat.czar95@gmail.com>
2024-08-01 15:50:58 +0200

    feat(EAT-10): create Home Page Object)

--- Project2-Cypress/cypress.config.js
+++ Project2-Cypress/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://skleptest.pl',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ Project2-Cypress/cypress/pages/homePages.js
@@ -0,0 +1,21 @@
+class HomePage {
+      
+    goToRegistration() {
+      cy.get('a[href="/my-account"]').click();
+    }
+  
+    goToLogin() {
+      cy.get('a[href="/my-account"]').click();
+    }
+  
+    goToProducts() {
+        cy.visit('/');
+    }
+  
+    goToCart() {
+      cy.get('a[href="/cart"]').click();
+    }
+  }
+  
+  export default HomePage;
+  
